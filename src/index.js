3b5146657939ae78c7fef1ef613e11217ab3bf5b
deployAt <mikowhy@tutanota.com>
2020-12-05 23:48:44 +0100

    feat: seo improvements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { hydrate, render } from 'react-dom'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import { Main } from './main'
 
-ReactDOM.render(<Main />, document.getElementById('root'))
+const rootElement = document.getElementById('root')
+if (rootElement.hasChildNodes()) {
+  hydrate(<Main />, rootElement)
+} else {
+  render(<Main />, rootElement)
+}
